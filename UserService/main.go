60fb03073debaeb40dd518dce39bfc303a85aa05
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2024-08-16 22:15:07 -0300

    Initialize HTTP server and integrate basic user routes

--- UserService/main.go
+++ UserService/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"User_Service/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.RoutersGenerator()
+	fmt.Printf("Starting server on: %d", 8080)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", 8080), r))
+}
